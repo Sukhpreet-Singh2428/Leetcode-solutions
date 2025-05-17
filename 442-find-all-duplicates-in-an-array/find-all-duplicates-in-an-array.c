ef488f9cdcb69baf08a2b4460ca7e711936b155d
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2025-05-17 11:33:47 +0530

    Time: 0 ms (100.00%) | Memory: 24.3 MB (87.25%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.c
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.c
@@ -2,13 +2,13 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* findDuplicates(int* nums, int numsSize, int* returnSize) {
-    int freq[1000001] = {0};
+    int freq[100001] = {0};
     for(int i=0;i<numsSize;i++){
         freq[nums[i]]++;
     }
     int* result = (int*)malloc(numsSize*sizeof(int));
     int j = 0;
-    for(int i=0;i<1000001;i++){
+    for(int i=0;i<100001;i++){
         if(freq[i]>1) result[j++] = i;
     }
     *returnSize = j;
