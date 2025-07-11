e440764aa5767345f858e2559acf22ea9917d2d3
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2025-07-11 10:52:30 +0530

    Time: 53 ms (44.93%) | Memory: 90.7 MB (61.14%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,10 +3,8 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> st;
         for(int i=0; i<nums.size(); i++){
-            if(st.find(nums[i]) == st.end()){
-                st.insert(nums[i]);
-            }
-            else return true;
+            if(st.find(nums[i]) != st.end()) return true;
+            else st.insert(nums[i]);
         }
         return false;
     }
