8b7bc606e1c1205f93ac82e5d9d0d819fa95a9a3
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2025-07-13 18:45:14 +0530

    Time: 4 ms (48.63%) | Memory: 11.7 MB (90.22%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int> mpp;
+        // unordered_map<char, int> mpp;
+        // for(auto it : magazine){
+        //     mpp[it]++;
+        // }
+        // for(auto it : ransomNote){
+        //     if(mpp[it]==0) return false;
+        //     mpp[it]--;
+        // }
+        // return true;
+
+        vector<int> hash(26,0);
         for(auto it : magazine){
-            mpp[it]++;
+            hash[it - 'a']++;
         }
         for(auto it : ransomNote){
-            if(mpp[it]==0) return false;
-            mpp[it]--;
+            if(hash[it - 'a']==0) return false;
+            hash[it - 'a']--;
         }
         return true;
     }
