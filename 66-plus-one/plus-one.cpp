c117cc6a52a33301fd12dad7fc5e9a93e89a5b30
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2025-07-25 15:05:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (34.67%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        // long long num = 0;
-        // for(auto it:digits){
-        //     num = num*10 + it;
-        // }
-        // num = num + 1;
-        // vector<int> result;
-        // while(num>0){
-        //     result.push_back(num%10);
-        //     num /= 10;
-        // }
-        // reverse(result.begin(),result.end());
-        // return result;
-
         int n = digits.size();
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
