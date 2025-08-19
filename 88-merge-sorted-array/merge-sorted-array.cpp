bdd54fa2815d4843ec5f97f33b8adc20af6f4f44
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2025-08-19 11:30:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (95.72%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,29 +1,48 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int left = 0;
+        // int left = 0;
+        // int right = 0;
+        // vector<int> ans;
+        // while(left<m && right<n){
+        //     if(nums1[left]<=nums2[right]){
+        //         ans.push_back(nums1[left]);
+        //         left++;
+        //     }
+        //     else{
+        //         ans.push_back(nums2[right]);
+        //         right++;
+        //     }
+        // }
+        // while(left<m){
+        //     ans.push_back(nums1[left]);
+        //     left++;
+        // }
+        // while(right<n){
+        //     ans.push_back(nums2[right]);
+        //     right++;
+        // }
+        // for(int i=0; i<m+n; i++){
+        //     nums1[i] = ans[i];
+        // }
+
+        int left = m-1;
         int right = 0;
-        vector<int> ans;
-        while(left<m && right<n){
-            if(nums1[left]<=nums2[right]){
-                ans.push_back(nums1[left]);
-                left++;
-            }
-            else{
-                ans.push_back(nums2[right]);
+        while(left>=0 && right<n){
+            if(nums1[left] > nums2[right]){
+                swap(nums1[left], nums2[right]);
+                left--;
                 right++;
             }
+            else{
+                break;
+            }
         }
-        while(left<m){
-            ans.push_back(nums1[left]);
-            left++;
-        }
-        while(right<n){
-            ans.push_back(nums2[right]);
-            right++;
-        }
-        for(int i=0; i<m+n; i++){
-            nums1[i] = ans[i];
+        sort(nums1.begin()+0, nums1.begin()+m);
+        sort(nums2.begin(), nums2.end());
+
+        for(int i=m; i<m+n; i++){
+            nums1[i] = nums2[i-m];
         }
     }
 };
