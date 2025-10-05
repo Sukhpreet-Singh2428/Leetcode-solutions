d0a83f9a2d41aaae55290470e95adbadc1cba4c4
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2025-10-05 19:56:14 +0530

    Time: 54 ms (27.51%) | Memory: 61.7 MB (66.20%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canWePlace(vector<int>& position, int dist, int m){
+        int n = position.size();
+
+        int cntBalls = 1;
+        int last = position[0];
+        for(int i=0; i<n; i++){
+            if( (position[i] - last) >= dist ){
+                cntBalls++;
+                last = position[i];
+            }
+            if(cntBalls >= m) return true;
+        }
+        return false;
+    }
+    int maxDistance(vector<int>& position, int m) {
+        int n = position.size();
+
+        sort(position.begin(), position.end());
+        int maxi = position[n-1];
+        int mini = position[0];
+        int low = 1;
+        int high = maxi-mini;
+        int ans = -1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(canWePlace(position, mid, m) == true){
+                ans = mid;
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+};
