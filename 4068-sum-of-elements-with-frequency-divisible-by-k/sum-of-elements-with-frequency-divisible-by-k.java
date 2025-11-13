d7e673c2ef2172356f0a34dfc71b04d75c9c40a6
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2025-11-13 15:49:50 +0530

    Time: 5 ms (2.39%) | Memory: 46.3 MB (6.94%) - LeetSync

--- 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.java
+++ 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.java
@@ -12,21 +12,5 @@ class Solution {
             }
         }
         return sum;
-
-
-        // HashMap<Integer, Integer> mp = new HashMap<>();
-
-        // for(int i=0; i<nums.length; i++){
-        //     mp.put(nums[i], mp.getOrDefault(nums[i], 0)+1);
-        // }
-        // int sum = 0;
-        // for(Map.Entry<Integer, Integer> entry : mp.entrySet()){
-        //     if(entry.getValue() % k == 0){
-        //         sum += entry.getKey() * entry.getValue();
-        //     }
-        // }
-
-        // if(sum % k == 0) return sum;
-        // return 0;
     }
 }
