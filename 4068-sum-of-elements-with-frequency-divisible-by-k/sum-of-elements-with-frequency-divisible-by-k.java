8c83f880117c69bef360d06e19beab72bca02ed7
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2025-11-13 15:49:00 +0530

    Time: 5 ms (2.39%) | Memory: 46.2 MB (6.94%) - LeetSync

--- 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.java
+++ 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.java
@@ -1,18 +1,32 @@
 class Solution {
     public int sumDivisibleByK(int[] nums, int k) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
 
-        for(int i=0; i<nums.length; i++){
-            mp.put(nums[i], mp.getOrDefault(nums[i], 0)+1);
+        int[] freq = new int[1001];
+        for(int num : nums){
+            freq[num]++;
         }
         int sum = 0;
-        for(Map.Entry<Integer, Integer> entry : mp.entrySet()){
-            if(entry.getValue() % k == 0){
-                sum += entry.getKey() * entry.getValue();
+        for(int i=0; i<freq.length; i++){
+            if(freq[i] % k == 0){
+                sum += i * freq[i];
             }
         }
+        return sum;
 
-        if(sum % k == 0) return sum;
-        return 0;
+
+        // HashMap<Integer, Integer> mp = new HashMap<>();
+
+        // for(int i=0; i<nums.length; i++){
+        //     mp.put(nums[i], mp.getOrDefault(nums[i], 0)+1);
+        // }
+        // int sum = 0;
+        // for(Map.Entry<Integer, Integer> entry : mp.entrySet()){
+        //     if(entry.getValue() % k == 0){
+        //         sum += entry.getKey() * entry.getValue();
+        //     }
+        // }
+
+        // if(sum % k == 0) return sum;
+        // return 0;
     }
 }
