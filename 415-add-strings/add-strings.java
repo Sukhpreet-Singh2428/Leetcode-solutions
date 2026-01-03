7014b918d1a20c9622ea1585ed72c70dc27d8ca3
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2026-01-03 18:43:57 +0530

    Time: 3 ms (31.73%) | Memory: 43.5 MB (94.23%) - LeetSync

---
+++ 415-add-strings/add-strings.java
@@ -0,0 +1,61 @@
+class Solution {
+    public static void reverse(char[] arr){
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public static void reverse(StringBuilder ans){
+        int i = 0;
+        int j = ans.length()-1;
+        while(i<j){
+            char temp = ans.charAt(i);
+            ans.setCharAt(i, ans.charAt(j));
+            ans.setCharAt(j, temp);
+            i++;
+            j--;
+        }
+    }
+    public String addStrings(String num1, String num2) {
+        char[] first = num1.toCharArray();
+        char[] second = num2.toCharArray();
+        
+        reverse(first);
+        reverse(second);
+        
+        StringBuilder ans = new StringBuilder();
+        int carry = 0;
+        int i = 0;
+        int j = 0;
+        int n = first.length;
+        int m = second.length;
+        while(i<n || j<m){
+            int sum = 0;
+            if(i<n) sum += first[i] - '0';
+            if(j<m) sum += second[j] - '0';
+            sum += carry;
+            ans.append(sum%10);
+            carry = sum/10;
+            
+            if(i<n) i++;
+            if(j<m) j++;
+        }
+        if(carry != 0){
+            ans.append(carry);
+        }
+        
+        reverse(ans);
+        
+        int x = 0;
+        while(x<ans.length() && ans.charAt(x) == '0'){
+            x++;
+        }
+        if(x>=ans.length()) return "0";
+        return ans.substring(x);
+    }
+}
