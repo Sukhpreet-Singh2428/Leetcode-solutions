c23373cdef32e27db465f3a4710370b0583eb7d0
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2026-01-14 20:37:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (18.44%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -1,16 +1,23 @@
 class Solution {
     public int prefixCount(String[] words, String pref) {
-        int cnt = 0;
-        for(int i=0; i<words.length; i++){
-            String s = words[i];
-            if(s.length()<pref.length()) continue;
-            int j=0;
-            while(j<pref.length() && s.charAt(j)==pref.charAt(j)){
-                j++;
-            }
-            if(j==pref.length()) cnt++;
-        }
+        // int cnt = 0;
+        // for(int i=0; i<words.length; i++){
+        //     String s = words[i];
+        //     if(s.length()<pref.length()) continue;
+        //     int j=0;
+        //     while(j<pref.length() && s.charAt(j)==pref.charAt(j)){
+        //         j++;
+        //     }
+        //     if(j==pref.length()) cnt++;
+        // }
 
+        // return cnt;
+
+
+        int cnt = 0;
+        for(String s : words){
+            if(s.startsWith(pref)) cnt++;
+        }
         return cnt;
     }
 }
