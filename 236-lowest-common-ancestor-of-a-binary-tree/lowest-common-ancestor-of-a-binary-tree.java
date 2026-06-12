1bb2ea0268a8be1a832fd20e1367e45917c8c51b
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2026-06-12 19:52:00 +0530

    Time: 15 ms (10.17%) | Memory: 70 MB (5.04%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,45 +8,26 @@
  * }
  */
 class Solution {
-    public static int func(TreeNode root, TreeNode target, List<TreeNode> ls){
-        if(root==null){
-            return 0;
-        }
-        if(root.val==target.val){
-            ls.add(root);
-            return 1;
-        }
-
-        ls.add(root);
-        int l = func(root.left, target, ls);
-        if(l==1) return 1;
-        int r = func(root.right, target, ls);
-        if(r==1) return 1;
-        ls.remove(ls.size()-1);
-
-        return 0;
-    }
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root.val==p.val || root.val==q.val){
+    public TreeNode func(TreeNode root, TreeNode p, TreeNode q){
+        if(root==null || root==p || root==q){
             return root;
         }
 
-        List<TreeNode> ls1 = new ArrayList<>();
-        func(root, p, ls1);
+        TreeNode left = func(root.left, p, q);
+        TreeNode right = func(root.right, p, q);
 
-        List<TreeNode> ls2 = new ArrayList<>();
-        func(root, q, ls2);
-
-        TreeNode ans = null;
-        int i=0;
-        int j=0;
-        while(i<ls1.size() && j<ls2.size()){
-            if(ls1.get(i).val==ls2.get(j).val){
-                ans = ls1.get(i);
-            }
-            i++;
-            j++;
+        if(left==null){
+            return right;
         }
+        else if(right==null){
+            return left;
+        }
+        else{
+            return root;
+        }
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ans = func(root, p, q);
 
         return ans;
     }
