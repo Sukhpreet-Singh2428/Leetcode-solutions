437a49286fde7debc5958cf37d518a1a1b02a93c
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2026-07-29 09:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (26.05%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.java
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -1,64 +1,16 @@
-class Pair{
-    int row;
-    int col;
-
-    public Pair(int row, int col){
-        this.row = row;
-        this.col = col;
-    }
-}
 class Solution {
-    public boolean bfs(int[][] matrix){
+    public boolean isToeplitzMatrix(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        Queue<Pair> qu = new ArrayDeque<>();
-
-        // Diagonals starting from the first row
-        for(int j=0; j<m; j++){
-            HashSet<Integer> st = new HashSet<>();
-            qu.offer(new Pair(0, j));
-
-            while(!qu.isEmpty()){
-                int r = qu.peek().row;
-                int c = qu.peek().col;
-                st.add(matrix[r][c]);
-                if(st.size() > 1) return false; 
-                qu.poll();
-
-                int drow = r+1;
-                int dcol = c+1;
-                if(drow<n && dcol<m){
-                    qu.offer(new Pair(drow, dcol));
-                }
-            }
-        }
-
-        // Diagonals starting from the first col
-        for(int i=0; i<n; i++){
-            HashSet<Integer> st = new HashSet<>();
-            qu.offer(new Pair(i, 0));
-
-            while(!qu.isEmpty()){
-                int r = qu.peek().row;
-                int c = qu.peek().col;
-                st.add(matrix[r][c]);
-                if(st.size() > 1) return false; 
-                qu.poll();
-
-                int drow = r+1;
-                int dcol = c+1;
-                if(drow<n && dcol<m){
-                    qu.offer(new Pair(drow, dcol));
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
                 }
             }
         }
 
         return true;
-    }
-    public boolean isToeplitzMatrix(int[][] matrix) {
-        boolean ans = bfs(matrix);
-
-        return ans;
     }
 }
