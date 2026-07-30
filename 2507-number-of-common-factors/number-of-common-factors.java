a1c3a47e5eedec67a0caf75ffb47ae2acdcb87ec
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2026-07-30 14:31:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (89.55%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int gcd(int a, int b){
+        while(a!=0 && b!=0){
+            if(a>b) a = a%b;
+            else b = b%a;
+        }
+        if(a==0) return b;
+        return a;
+    }
+    public int commonFactors(int a, int b) {
+        int x = gcd(a, b);
+
+        int cnt = 0;
+        for(int i=1; i<=x; i++){
+            if(a%i==0 && b%i==0) cnt++;
+        }
+
+        return cnt;
+    }
+}
